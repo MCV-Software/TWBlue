836675bfa15f58293a50d436d0b6a67499f92630
jmdaweb <jmdaweb@hotmail.com>
2014-12-16 22:12:29 +0100

    New sound for trending topics, updated contributors in application.py

--- src/application.py
+++ src/application.py
@@ -11,7 +11,7 @@ author = u"Manuel Cortéz"
 authorEmail = "manuel@manuelcortez.net"
 copyright = u"copyright (C) 2013-2014, Manuel cortéz"
 description = u"TW Blue is an app designed to use Twitter in a simple and fast way and avoiding, as far as possible, the consumtion of excessive resources of the machine where it’s running. With this app you’ll have access to most twitter features."
-translators = [u"Bryner Villalobos (English)", u"Mohammed Al Shara (Arabic)", u"Salva Doménech, Juan Carlos Rivilla(Catalan)", u"Manuel cortéz(Spanish)", u"Sukil Etxenike Arizaleta(Basque)", u"Jani Kinnunen(finnish)", u"Javier Currás, José Manuel Delicado, Alba Quinteiro(Galician)", u"Robert Osztolykan(Hungarian)", u"Paweł Masarczyk(Polish)", u"Odenilton Júnior Santos(Portuguese)", u"Alexander Jaszyn(Russian)", u"Burak (Turkish)"]
+translators = [u"Bryner Villalobos (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla(Catalan)", u"Manuel cortéz(Spanish)", u"Sukil Etxenike Arizaleta(Basque)", u"Jani Kinnunen(finnish)", u"Alba Quinteiro(Galician)", u"Robert Osztolykan(Hungarian)", u"Paweł Masarczyk(Polish)", u"Odenilton Júnior Santos(Portuguese)", u"Alexander Jaszyn(Russian)", u"Burak (Turkish)"]
 url = u"http://twblue.com.mx"
 report_bugs_url = "http://twblue.com.mx/bugs/api/soap/mantisconnect.php?wsdl"
 
