ca288de5155e430068f9303a3ebb336d480b45b1
Manuel Cortéz <manuel@manuelcortez.net>
2014-12-29 14:35:08 -0600

    next-gen: updated widgetUtils

--- src/widgetUtils/__init__.py
+++ src/widgetUtils/__init__.py
@@ -1,3 +1,6 @@
-import platform
-if platform.system() == "Windows":
- from wxUtils import *
+#import platform
+#if platform.system() == "Windows":
+from wxUtils import *
+from baseDialog import *
+#elif platform.system() == "Linux":
+#	from gtkUtils import *
--- src/widgetUtils/gtkUtils.py
+++ src/widgetUtils/gtkUtils.py
@@ -1,38 +1,38 @@
-import wx
+from gi.repository import Gtk, Gdk
 
-# Code responses for WX dialogs.
+# Code responses for GTK +3 dialogs.
 # this is when an user presses OK on a dialogue.
-OK = wx.ID_OK
+OK = Gtk.ResponseType.OK
 # This is when an user presses cancel on a dialogue.
-CANCEL = wx.ID_CANCEL
+CANCEL = Gtk.ResponseType.CANCEL
 # This is when an user closes the dialogue or an id to create the close button.
-CLOSE = wx.ID_CLOSE
+CLOSE = Gtk.ResponseType.CLOSE
 # The response for a "yes" Button pressed on a dialogue.
-YES = wx.ID_YES
+YES = Gtk.ResponseType.YES
 # This is when the user presses No on a default dialogue.
-NO = wx.ID_NO
+NO = Gtk.ResponseType.NO
 
 #events
 # This is raised when the application must be closed.
-CLOSE_EVENT = wx.EVT_CLOSE
+CLOSE_EVENT = "delete-event"
 # This is activated when a button  is pressed.
-BUTTON_PRESSED = wx.EVT_BUTTON
+BUTTON_PRESSED = "clicked"
 # This is activated when an user enter text on an edit box.
-ENTERED_TEXT = wx.EVT_TEXT
-MENU = wx.EVT_MENU
-KEYPRESS = wx.EVT_CHAR_HOOK
-KEYUP = wx.EVT_KEY_UP
-NOTEBOOK_PAGE_CHANGED = wx.EVT_NOTEBOOK_PAGE_CHANGED
+#ENTERED_TEXT = wx.EVT_TEXT
+MENU = "activate"
+
+#KEYPRESS = wx.EVT_CHAR_HOOK
+#NOTEBOOK_PAGE_CHANGED = wx.EVT_NOTEBOOK_PAGE_CHANGED
 def exit_application():
  """ Closes the current window cleanly. """
- wx.GetApp().ExitMainLoop()
+ Gtk.main_quit()
 
 def connect_event(parent, event, func, menuitem=None, *args, **kwargs):
  """ Connects an event to a function.
-  parent wx.window: The widget that will listen for the event.
+  parent Gtk.widget: The widget that will listen for the event.
   event widgetUtils.event: The event that will be listened for the parent. The event should be one of the widgetUtils events.
   function func: The function that will be connected to the event."""
  if menuitem == None:
-  return getattr(parent, "Bind")(event, func, *args, **kwargs)
+  return getattr(parent, "connect")(event, func, *args, **kwargs)
  else:
-  return getattr(parent, "Bind")(event, func, menuitem, *args, **kwargs)
+  return getattr(menuitem, "connect")(event, func, *args, **kwargs)
