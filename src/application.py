3a6849cf0b28404bea5e324ce3a5c792cdf40de0
Manuel Cortéz <manuel@manuelcortez.net>
2015-01-08 08:09:20 -0600

    the Webdomain for TWBlue has been changed

--- src/application.py
+++ src/application.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-name = 'TW Blue'
+name = 'TWBlue'
 snapshot = False
 if snapshot == False:
- version = "0.50"
- update_url = 'http://twblue.com.mx/updates/tw_blue.json'
+ version = "0.52"
+ update_url = 'http://twblue.es/updates/tw_blue.json'
 else:
- version = "6"
- update_url = 'http://twblue.com.mx/updates/snapshots.json'
+ version = "7"
+ update_url = 'http://twblue.es/updates/snapshots.json'
 author = u"Manuel Cortéz"
 authorEmail = "manuel@manuelcortez.net"
 copyright = u"copyright (C) 2013-2014, Manuel cortéz"
 description = u"TW Blue is an app designed to use Twitter in a simple and fast way and avoiding, as far as possible, the consumtion of excessive resources of the machine where it’s running. With this app you’ll have access to most twitter features."
 translators = [u"Bryner Villalobos (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla(Catalan)", u"Manuel cortéz(Spanish)", u"Sukil Etxenike Arizaleta(Basque)", u"Jani Kinnunen(finnish)", u"Alba Quinteiro(Galician)", u"Robert Osztolykan(Hungarian)", u"Paweł Masarczyk(Polish)", u"Odenilton Júnior Santos(Portuguese)", u"Alexander Jaszyn(Russian)", u"Burak (Turkish)"]
-url = u"http://twblue.com.mx"
-report_bugs_url = "http://twblue.com.mx/bugs/api/soap/mantisconnect.php?wsdl"
+url = u"http://twblue.es"
+report_bugs_url = "http://twblue.es/bugs/api/soap/mantisconnect.php?wsdl"
 
 # Tokens
 app_key = '8pDLbyOW3saYnvSZ4uLFg'
