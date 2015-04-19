7dd383b15e3a9ed26b459ac2fcc3da3b51c39430
Bill Dengler <codeofdusk@gmail.com>
2015-04-19 19:33:22 -0400

    more app name removal.

--- src/application.py
+++ src/application.py
@@ -10,7 +10,7 @@ else:
 author = u"Manuel Cortéz"
 authorEmail = "manuel@manuelcortez.net"
 copyright = u"copyright (C) 2013-2015, Manuel cortéz"
-description = u"TW Blue is an app designed to use Twitter simply and efficiently while using minimal system resources. This app provides access to most Twitter features."
+description = unicode(name+" is an app designed to use Twitter simply and efficiently while using minimal system resources. This app provides access to most Twitter features.")
 translators = [u"Bryner Villalobos, Bill Dengler (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla (Catalan)", u"Manuel cortéz (Spanish)", u"Sukil Etxenike Arizaleta (Basque)", u"Jani Kinnunen (finnish)", u"Rémy Ruiz (French)", u"Alba Quinteiro (Galician)", u"Steffen Schultz (German)", u"Robert Osztolykan (Hungarian)", u"Paweł Masarczyk (Polish)", u"Odenilton Júnior Santos (Portuguese)", u"Alexander Jaszyn (Russian)", u"Burak (Turkish)"]
 url = u"http://twblue.es"
 report_bugs_url = "http://twblue.es/bugs/api/soap/mantisconnect.php?wsdl"
