83f9e32fd65ec44a55ace0c411921eea59ba908d
Bill Dengler <codeofdusk@gmail.com>
2015-05-02 04:29:41 -0400

    Corrected word choice.

--- src/wxUI/commonMessageDialogs.py
+++ src/wxUI/commonMessageDialogs.py
@@ -5,7 +5,7 @@ def retweet_question(parent):
  return wx.MessageDialog(parent, _(u"Would you like to add a comment to this tweet?"), _("Retweet"), wx.YES_NO|wx.CANCEL|wx.ICON_QUESTION).ShowModal()
 
 def delete_tweet_dialog(parent):
- return wx.MessageDialog(parent, _(u"Do you really want to delete this message? It will be eliminated from Twitter as well."), _(u"Delete"), wx.ICON_QUESTION|wx.YES_NO).ShowModal()
+ return wx.MessageDialog(parent, _(u"Do you really want to delete this message? It will be deleted from Twitter as well."), _(u"Delete"), wx.ICON_QUESTION|wx.YES_NO).ShowModal()
 
 def exit_dialog(parent):
  dlg = wx.MessageDialog(parent, _(u"Do you really want to close " + application.name + "?"), _(u"Exit"), wx.YES_NO|wx.ICON_QUESTION)
