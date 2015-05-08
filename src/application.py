e6ce5532168658be832f11dbad9d0f969bd73285
Bill Dengler <codeofdusk@gmail.com>
2015-05-08 10:04:37 -0400

    Updated application info, set bug reporting URL to new page for #26.

--- src/application.py
+++ src/application.py
@@ -7,10 +7,10 @@ if snapshot == False:
 else:
  version = "9.2"
  update_url = 'http://twblue.es/updates/snapshots_ngen.json'
-author = u"Manuel Cortéz"
+author = u"Manuel Cortéz, Bill Dengler"
 authorEmail = "manuel@manuelcortez.net"
-copyright = u"copyright (C) 2013-2015, Manuel cortéz"
+copyright = u"copyright (C) 2013-2015, Manuel cortéz and Bill Dengler"
 description = unicode(name+" is an app designed to use Twitter simply and efficiently while using minimal system resources. This app provides access to most Twitter features.")
 translators = [u"Bryner Villalobos, Bill Dengler (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla (Catalan)", u"Manuel cortéz (Spanish)", u"Sukil Etxenike Arizaleta (Basque)", u"Jani Kinnunen (finnish)", u"Rémy Ruiz (French)", u"Alba Quinteiro (Galician)", u"Steffen Schultz (German)", u"Robert Osztolykan (Hungarian)", u"Paweł Masarczyk (Polish)", u"Odenilton Júnior Santos (Portuguese)", u"Alexander Jaszyn (Russian)", u"Burak (Turkish)"]
 url = u"http://twblue.es"
-report_bugs_url = "http://twblue.es/bugs/api/soap/mantisconnect.php?wsdl"
+report_bugs_url = "http://github.com/manuelcortez/twblue/issues/new"
