8e27e9720c03cb7764d3eaff9ee3f8c9640d04be
Manuel Cortéz <manuel@manuelcortez.net>
2015-05-08 09:38:00 -0500

    Closed #26

--- src/application.py
+++ src/application.py
@@ -13,4 +13,4 @@ copyright = u"copyright (C) 2013-2015, Manuel cortéz and Bill Dengler"
 description = unicode(name+" is an app designed to use Twitter simply and efficiently while using minimal system resources. This app provides access to most Twitter features.")
 translators = [u"Bryner Villalobos, Bill Dengler (English)", u"Mohammed Al Shara (Arabic)", u"Joan Rabat, Juan Carlos Rivilla (Catalan)", u"Manuel cortéz (Spanish)", u"Sukil Etxenike Arizaleta (Basque)", u"Jani Kinnunen (finnish)", u"Rémy Ruiz (French)", u"Alba Quinteiro (Galician)", u"Steffen Schultz (German)", u"Robert Osztolykan (Hungarian)", u"Paweł Masarczyk (Polish)", u"Odenilton Júnior Santos (Portuguese)", u"Alexander Jaszyn (Russian)", u"Burak (Turkish)"]
 url = u"http://twblue.es"
-report_bugs_url = "http://github.com/manuelcortez/twblue/issues/new"
+report_bugs_url = "http://twblue.es/bugs/api/soap/mantisconnect.php?wsdl"
