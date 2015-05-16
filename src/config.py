dc5d11c1c8a12519714a8df43237e71f96e29f7d
Bill Dengler <codeofdusk@gmail.com>
2015-05-16 01:13:54 -0400

    #41: Add load_keymap config entry, load a keymap as config.keymap.

--- src/config.py
+++ src/config.py
@@ -9,8 +9,11 @@ MAINFILE = "twblue.conf"
 MAINSPEC = "app-configuration.defaults"
 
 app = None
-
+keymap=None
 def setup ():
  global app
  log.debug("Loading global app settings...")
  app = config_utils.load_config(paths.config_path(MAINFILE), paths.app_path(MAINSPEC))
+ log.debug("Loading keymap...")
+ global keymap
+ keymap = config_utils.load_config(paths.app_path("keymaps/"+app['app-settings']['load_keymap']), paths.app_path('keymaps/base.template'))
