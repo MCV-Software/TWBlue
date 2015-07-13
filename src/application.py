0f49bfba2e07f0c73e6e28525ef56bb87a03cade
Manuel Cortéz <manuel@manuelcortez.net>
2015-07-13 08:25:30 -0500

    Snapshot 9.

--- src/application.py
+++ src/application.py
@@ -5,7 +5,7 @@ if snapshot == False:
  version = "0.80"
  update_url = 'http://twblue.es/updates/twblue_ngen.json'
 else:
- version = "9.8"
+ version = "9.9"
  update_url = 'http://twblue.es/updates/snapshots_ngen.json'
 author = u"Manuel Cortéz, Bill Dengler"
 authorEmail = "manuel@manuelcortez.net"
