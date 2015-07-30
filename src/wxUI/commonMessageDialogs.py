692d75562e8ee9e07fbb738ea8b6fabe48c9b98e
Bill Dengler <codeofdusk@gmail.com>
2015-07-30 14:03:52 -0400

    String cleanup.

--- src/wxUI/commonMessageDialogs.py
+++ src/wxUI/commonMessageDialogs.py
@@ -49,5 +49,5 @@ def no_following():
  return wx.MessageDialog(None, _(u"This is a protected user account, you need follow to this user for viewing your tweets or favourites."), _(u"Error"), wx.ICON_ERROR).ShowModal()
 
 def donation():
- dlg = wx.MessageDialog(None, _(u"If you like this application, we need your help to keep it operational. Help us donating to the project. This will let us pay the server, the domain on the Internet and some other things to ensure that TWBlue will keep developing. Your donation will let us continue writing features for TWBlue, and these features to be free. Would you like to donate now?"), _(u"We need your help"), wx.ICON_QUESTION|wx.YES_NO)
+ dlg = wx.MessageDialog(None, _(u"If you like " + application.name + ", we need your help to keep it going. Help us by donating to the project. This will help us pay for the server, the domain and some other things to ensure that " + application.name + " will be actively maintained. Your donation will give us the means to continue the development of " + application.name + ", and to keep " + application.name + " free. Would you like to donate now?"), _(u"We need your help"), wx.ICON_QUESTION|wx.YES_NO)
  return dlg.ShowModal()
