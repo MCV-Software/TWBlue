76582c6313255644400134d11c0d5cf60b3ffd97
=Manuel Cortez <manuel@manuelcortez.net>
2017-07-30 04:05:32 -0500

    Show dialog while waiting for account authorisation. Possibly fixes #101. Needs tests

--- src/sessionmanager/wxUI.py
+++ src/sessionmanager/wxUI.py
@@ -74,3 +74,16 @@ class sessionManagerWindow(wx.Dialog):
 
  def destroy(self):
   self.Destroy()
+
+class authorisationDialog(wx.Dialog):
+ def __init__(self):
+  super(authorisationDialog, self).__init__(parent=None, title=_(u"Authorising account..."))
+  panel = wx.Panel(self)
+  sizer = wx.BoxSizer(wx.VERTICAL)
+  self.text = wx.TextCtrl(panel, -1, _("Waiting for account authorisation..."), style=wx.TE_READONLY|wx.TE_MULTILINE)
+  self.cancel = wx.Button(panel, wx.ID_CANCEL)
+  sizer.Add(self.text, 0, wx.ALL, 5)
+  sizer.Add(self.cancel, 0, wx.ALL, 5)
+  panel.SetSizer(sizer)
+  min = sizer.CalcMin()
+  self.SetClientSize(min)
