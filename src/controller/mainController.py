efc6843ee2e74108797c1a7266f57c508b11667e
Manuel Cortez <manuel@manuelcortez.net>
2017-11-10 12:01:43 -0600

    Redirects snapshot users to changelog hosted in the repository

--- src/controller/mainController.py
+++ src/controller/mainController.py
@@ -1521,10 +1521,13 @@ class Controller(object):
   os.chdir("../../")
 
  def view_changelog(self, *args, **kwargs):
-  lang = localization.get("documentation")
-  os.chdir("documentation/%s" % (lang,))
-  webbrowser.open("changelog.html")
-  os.chdir("../../")
+  if application.snapshot == True:
+   webbrowser.open("https://github.com/manuelcortez/twblue/blob/next-gen/doc/changelog.md")
+  else:
+   lang = localization.get("documentation")
+   os.chdir("documentation/%s" % (lang,))
+   webbrowser.open("changelog.html")
+   os.chdir("../../")
 
  def insert_buffer(self, buffer, position):
   self.buffers.insert(position, buffer)
