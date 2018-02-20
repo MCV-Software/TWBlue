fff215fcdb54da3ac8286c29278778b41dce5797
Manuel Cortez <manuel@manuelcortez.net>
2018-02-20 12:42:24 -0600

    Updated app info

--- src/application.py
+++ src/application.py
@@ -11,7 +11,7 @@ else:
  mirror_update_url = 'https://raw.githubusercontent.com/manuelcortez/TWBlue/next-gen/updates/snapshots.json'
 authors = [u"Manuel Cortéz", u"José Manuel Delicado"]
 authorEmail = "manuel@manuelcortez.net"
-copyright = u"Copyright (C) 2013-2017, Manuel cortéz."
+copyright = u"Copyright (C) 2013-2018, Manuel cortéz."
 description = unicode(name+" is an app designed to use Twitter simply and efficiently while using minimal system resources. This app provides access to most Twitter features.")
 translators = [u"Manuel Cortéz (English)", u"Mohammed Al Shara, Hatoun Felemban (Arabic)", u"Francisco Torres (Catalan)", u"Manuel cortéz (Spanish)", u"Sukil Etxenike Arizaleta (Basque)", u"Jani Kinnunen (finnish)", u"Rémy Ruiz (French)", u"Juan Buño (Galician)", u"Steffen Schultz (German)", u"Zvonimir Stanečić (Croatian)", u"Robert Osztolykan (Hungarian)", u"Christian Leo Mameli (Italian)", u"Riku (Japanese)", u"Paweł Masarczyk (Polish)", u"Odenilton Júnior Santos (Portuguese)", u"Florian Ionașcu, Nicușor Untilă (Romanian)", u"Natalia Hedlund, Valeria Kuznetsova (Russian)", u"Aleksandar Đurić (Serbian)", u"Burak Yüksek (Turkish)"]
 url = u"https://twblue.es"
