655331d405eff793d4341c47c03a535715f97b8b
Manuel Cortez <manuel@manuelcortez.net>
2018-08-15 11:22:12 -0500

    Updated script for generating translation templates

--- tools/genpot.bat
+++ tools/genpot.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Generating application translation strings...
-C:\python27\python.exe pygettext.py  -d twblue ../src/*.pyw ../src/*.py ../src/*/*.py ../src/*/*.pyw ../src/*/*/*.py ../src/*/*/*.pyw ../src/*/*/*/*.py ../src/*/*/*/*.pyw ../src/*/*/*/*/*.py ../src/*/*/*/*/*.pyw
+C:\python27\python.exe pygettext.py  -v -d twblue ../src/*.pyw ../src/*.py ../src/*/*.py ../src/*/*.pyw ../src/*/*/*.py ../src/*/*/*.pyw ../src/*/*/*/*.py ../src/*/*/*/*.pyw ../src/*/*/*/*/*.py ../src/*/*/*/*/*.pyw
 C:\python27\python.exe pygettext.py -v -d twblue-documentation ../doc/strings.py
 C:\python27\python.exe pygettext.py -v -d twblue-changelog ../doc/changelog.py
