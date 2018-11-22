16b2e1661451a9706f2600913ccc9d66cc2501c8
Manuel Cortez <manuel@manuelcortez.net>
2018-11-22 17:47:02 -0600

    Removed libloader from the source repository. #273

--- src/libloader/__init__.py
+++
@@ -1,7 +0,0 @@
-from .libloader import *
-
-__version__ = 0.1
-__author__ = 'Christopher Toth <q@q-continuum.net>'
-__doc__ = """
-Quickly and easily load shared libraries from various platforms. Also includes a libloader.com module for loading com modules on Windows.
-"""
