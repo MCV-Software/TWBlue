316a539a11fbb61c6a2b4db1e666bf31434b84aa
Manuel Cortez <manuel@manuelcortez.net>
2019-08-13 17:39:05 -0500

    Docs can be generated from python 3.7

--- doc/generator.py
+++ doc/generator.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
-import markdown
+import gettext
 import os
+import locale
+import paths
+import markdown
 import shutil
 from codecs import open as _open
-import languageHandler
-languageHandler.setLanguage("en")
-import strings
-import changelog
+from importlib import reload
+
+def change_language(name, language):
+ global _
+ os.environ["lang"] = language
+ _ = gettext.install(name, os.path.join(paths.app_path(), "locales"))
 
 # the list of supported language codes of TW Blue
 languages = ["en", "es", "fr", "de", "it", "gl", "ja", "ru", "ro", "eu", "ca", "da"]
-#"eu", "ar", "ca", "es", "fi", "fr", "gl", "hu", "it", "pl", "pt", "ru", "tr"]
 
 def generate_document(language, document_type="documentation"):
- reload(languageHandler)
  if document_type == "documentation":
   translation_file = "twblue-documentation"
-  languageHandler.setLanguage(language, translation_file)
+  change_language(translation_file, language)
   reload(strings)
   markdown_file = markdown.markdown("\n".join(strings.documentation[1:]), extensions=["markdown.extensions.toc"])
   title = strings.documentation[0]
   filename = "manual.html"
  elif document_type == "changelog":
   translation_file = "twblue-changelog"
-  languageHandler.setLanguage(language, translation_file)
+  change_language(translation_file, language)
   reload(changelog)
   markdown_file = markdown.markdown("\n".join(changelog.documentation[1:]), extensions=["markdown.extensions.toc"])
   title = changelog.documentation[0]
@@ -57,4 +60,7 @@ def create_documentation():
   generate_document(i, "changelog")
  print("Done")
 
+change_language("twblue-documentation", "en")
+import strings
+import changelog
 create_documentation()
