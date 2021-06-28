a9f52b3a94be8ec55306ee9679b33946e3b122cd
Manuel Cortez <manuel@manuelcortez.net>
2021-06-28 06:01:05 -0500

    Added convert_bytes function to show correctly FTP upload progress

--- scripts/upload.py
+++ scripts/upload.py
@@ -6,6 +6,21 @@ import ftplib
 
 transferred=0
 
+def convert_bytes(n):
+	K, M, G, T, P = 1 << 10, 1 << 20, 1 << 30, 1 << 40, 1 << 50
+	if   n >= P:
+		return '%.2fPb' % (float(n) / T)
+	elif   n >= T:
+		return '%.2fTb' % (float(n) / T)
+	elif n >= G:
+		return '%.2fGb' % (float(n) / G)
+	elif n >= M:
+		return '%.2fMb' % (float(n) / M)
+	elif n >= K:
+		return '%.2fKb' % (float(n) / K)
+	else:
+		return '%d' % n
+
 def callback(progress):
 	global transferred
 	transferred = transferred+len(progress)
