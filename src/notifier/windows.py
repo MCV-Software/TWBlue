93222417473f2b3069dfb5dc116daad6ad34b601
Manuel Cortez <manuel@manuelcortez.net>
2022-02-24 10:36:16 -0600

    Remove more unneeded code

--- src/notifier/windows.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from builtins import object
-import wx
-
-class notification(object):
-
-    def notify(self, title, text):
-        wx.NotificationMessage(title, text).Show()
