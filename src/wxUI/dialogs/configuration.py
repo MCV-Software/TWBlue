ab979e26237e0cb520915a091a9936a9387b50cd
Manuel Cortez <manuel@manuelcortez.net>
2022-05-13 13:04:12 -0500

    Added setting to hide emojis in usernames

--- src/wxUI/dialogs/configuration.py
+++ src/wxUI/dialogs/configuration.py
@@ -120,6 +120,8 @@ class generalAccount(wx.Panel, baseDialog.BaseWXDialog):
         sizer.Add(rMode, 0, wx.ALL, 5)
         self.show_screen_names = wx.CheckBox(self, wx.ID_ANY, _(U"Show screen names instead of full names"))
         sizer.Add(self.show_screen_names, 0, wx.ALL, 5)
+        self.hide_emojis = wx.CheckBox(self, wx.ID_ANY, _("hide emojis in usernames"))
+        sizer.Add(self.hide_emojis, 0, wx.ALL, 5)
         PersistSizeLabel = wx.StaticText(self, -1, _(u"Number of items per buffer to cache in database (0 to disable caching, blank for unlimited)"))
         self.persist_size = wx.TextCtrl(self, -1)
         sizer.Add(PersistSizeLabel, 0, wx.ALL, 5)
